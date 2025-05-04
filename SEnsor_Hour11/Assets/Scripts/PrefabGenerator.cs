07537434b1d3b8aa027ab38a8640d0f44e22a6e9
Sensor75 <samuel.ensor@gmail.com>
2025-05-04 16:13:12 -0700

    modified scripts

--- SEnsor_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ SEnsor_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -6,15 +6,16 @@ public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
     public int itemCount = 10;
-    public Vector3 startPosition;
-    public float spacing;
+    public float spacing = 3f;
 
     void Start()
     {
+        Vector3 startPosition = new Vector3(0, 1, 0); 
+
         for (int i = 0; i < itemCount; i++)
         {
-            Vector3 currentPosition = startPosition + Vector3.right * (i * spacing);
-            GameObject newObject = Instantiate(prefab, currentPosition, Quaternion.identity);
+            Vector3 position = startPosition + new Vector3(i * spacing, 0, 0); 
+            Instantiate(prefab, position, Quaternion.identity);
         }
     }
 
@@ -24,10 +25,10 @@ public class PrefabGenerator : MonoBehaviour
         {
             Instantiate(prefab);
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(prefab, transform.position, transform.rotation);
         }
     }
 }
-
