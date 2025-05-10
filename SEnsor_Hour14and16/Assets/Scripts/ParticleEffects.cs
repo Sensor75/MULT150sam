495933b5d90aff652cf29fc32f62687db17f135b
Sensor75 <samuel.ensor@gmail.com>
2025-05-10 11:13:22 -0700

    Worked with Particles

---
+++ SEnsor_Hour14and16/Assets/Scripts/ParticleEffects.cs
@@ -0,0 +1,7 @@
+/*
+Changed the Emission Speed to 25
+Changed the shape of the particle system to doughnut 
+Added a Point Light to the Particle system 
+Added a color over time effect 
+Added Ribbons Trails 
+*/
