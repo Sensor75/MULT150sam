862c302ad5ecc91421612b678c677711600b6573
Sensor75 <samuel.ensor@gmail.com>
2025-05-31 13:30:39 -0700

    FinalProject

---
+++ SENsor_ChasosBall_Final/Assets/Scripts/GameCritics.cs
@@ -0,0 +1,12 @@
+/*
+1 Directional Light intensity changed to 5 and color to a light yellow. 
+2 Added fox and changed the Color & Density.
+3 Added a custom skybox and changed it's intensity properties.
+4 Made prefabs fpr each of the chaos balls 
+5 Gave the chaos ball prefabs a particle system with intensity properites added 
+6 Converted the colored balls into prefabs 
+7 Applied one new texture to one of the balls.
+8 Modified the light components of the goals
+9 Changed the walls into complex objects 
+10 Used gameobject hierarchy and applied one new texture. 
+*/
